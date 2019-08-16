9fa90d52e21e6a4cf22d001e7c1e7ca69d2bc086
yuvalol <yuvalolshaker@gmail.com>
2019-08-16 14:58:40 +0300

    change to while start the game. flip cards

--- src/src/Main.java
+++ src/src/Main.java
@@ -6,6 +6,8 @@ public class Main {
     private static final int boardHeight = 5;
     private static final int boardWidth = 10;
     private static Board board;
+    private static final int second = 1000;
+    private static final int flipCounter = 2;
 
     public static void main(String args[]){
         createBoard();
@@ -14,18 +16,49 @@ public class Main {
 
     private static void runGame() {
         System.out.println(board);
-
-        for (int i=0; i<100; i++){
+        Point lastLocation = MouseInfo.getPointerInfo().getLocation();
+        Point currentLocation;
+        boolean game = true;
+        int counter = 0;
+        MemCard previousCard;
+        MemCard currentCard = null;
+        while (game) {
+            previousCard = currentCard;
             try {
-                Thread.sleep(1000);
-                double x = MouseInfo.getPointerInfo().getLocation().getX();
-                double y = MouseInfo.getPointerInfo().getLocation().getY();
+                Thread.sleep(second);
+                currentLocation = MouseInfo.getPointerInfo().getLocation();
+                // if same location of mouse for some time we flip
+                if (currentLocation.equals(lastLocation)){
+                    counter++;
+                    if (counter == flipCounter){
+                        currentCard = flipCurrentCard(currentLocation, previousCard);
+                        if (previousCard == null){ // we flipped the first in the pair
+                            continue;
+                        }
+                    }
+                }
 //                System.out.println("x is: " + x + " y is: " + y);
-                System.out.println("you are pointing at: " + board.getCard(x,y));
+                System.out.println("you are pointing at: " + board.getCard(currentLocation.getX(), currentLocation.getY()));
             } catch (InterruptedException e) {
+                game = false;
                 e.printStackTrace();
             }
         }
+    }
+
+    /**
+     * flips the card that the mouse points at. and returns it.
+     * @param currentLocation - the location of the mouse
+     * @param previousCard - the last card we were at
+     * @return the current card that the mouse points at. if already on display or the previous returns null
+     */
+    private static MemCard flipCurrentCard(Point currentLocation, MemCard previousCard) {
+        MemCard currentCard = board.getCard(currentLocation.getX(), currentLocation.getY());
+        if (currentCard.isOnDisplay() || previousCard == currentCard){
+            return null;
+        }
+        currentCard.flip();
+        return currentCard;
     }
 
     private static void createBoard() {
